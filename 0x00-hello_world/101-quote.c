ff4c60195494177348f7d666cc0ef28f5dcf3592
Santiago Neira <4373@holbertonschool.com>
2022-02-03 11:32:47 -0800

    task advanced 9 without puts and printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,10 @@
-#include<stdio.h>
+#include<unistd.h>
 /**
  *main - entry
- *return:0
+ *return:1
  */
 int main(void)
 {
-	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	long l = 14; 
-	long fd = 1; 
-	long syscall = 1; 
-	long ret = 0;
-     	__asm__ ( "syscall":"=a" (ret): "a" (syscall), "D" (fd), "S" (s), "d" (l));
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 } 
