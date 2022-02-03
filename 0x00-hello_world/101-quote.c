2c07b5acd4a7db878fd5a459c9fef14c34059daf
Santiago Neira <4373@holbertonschool.com>
2022-02-03 11:38:20 -0800

    task advanced 9 without puts and printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include<unistd.h>
 /**
- *main - entry
- *return:1
+ * main - Entry point
+ * return:1
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-} 
+}
