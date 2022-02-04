0a6fcccf09cfd25c87b1624f8e8ec95db46aec4c
Santiago Neira <4373@holbertonschool.com>
2022-02-04 07:14:39 -0800

    lower

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ *  *main - entry point
+ *  *Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
