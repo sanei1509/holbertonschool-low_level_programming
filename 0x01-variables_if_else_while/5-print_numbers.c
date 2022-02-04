7c0163ae84b2c7456f7c6b24bb19ac8d7485a523
Santiago Neira <4373@holbertonschool.com>
2022-02-04 08:49:16 -0800

    print numbers base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ *main - entry point
+ *Return: 0
+ */
+int main(void)
+{
+	int ch = 0;
+
+	for (; ch < 10; ch++)
+	printf("%d", ch);
+	putchar('\n');
+	return (0);
+}
