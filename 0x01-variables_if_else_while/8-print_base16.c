285f0a0cc4c4391884182e06f4ce33e55c118ea5
Santiago Neira <4373@holbertonschool.com>
2022-02-04 10:21:11 -0800

    task 8 base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ *main - print the alphabet a-f and before zero to nine
+ *Return: 0
+ */
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	putchar(num);	
+	for (num = 'a'; num <= 'f'; num++)
+	{
+	putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
