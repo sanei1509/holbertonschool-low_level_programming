7308c25419968fe454ff2d07f3b56c3a5560be26
Santiago Neira <4373@holbertonschool.com>
2022-02-04 12:20:37 -0800

    task 10 combination of numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ *main - print the number zero to nine separate with coma
+ *Return: 0
+ */
+int main(void)
+{
+	int num;
+	int num1;
+
+	for (num = 0; num <= 9; num++)
+	{
+		for (num1 = (num + 1); num1 <= 9; num1++)
+		{
+			putchar(num + '0');
+			putchar(num1 + '0');
+			if( num != 8 )
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
