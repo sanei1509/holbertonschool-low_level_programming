3fdd0c329b9165334ab66163472b58ba1165da73
Santiago Neira <4373@holbertonschool.com>
2022-02-07 14:02:17 -0800

    task 9 tables 0 to 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,6 +6,52 @@
  *@c: number for evaluate
  */
 void times_table(void)
-{
-	
+{	
+	int res = 0;
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			res= j * i;
+			if (res > 9)
+			{
+				if (j != 0)
+				{
+					_putchar(' ');
+			 	}
+					_putchar((res / 10) + 48);
+					_putchar((res % 10) + 48);
+					if (j != 9)
+					{
+						if(res < 9)
+						{
+						_putchar(',');
+						_putchar(' ');
+						}
+						else
+						{
+						_putchar(',');
+						}
+					}
+			}
+			else
+			{
+					if (j != 0)
+					{
+						_putchar(' ');
+						_putchar(' ');
+					}
+			_putchar(res + 48);
+					if (j != 9)
+					{
+						_putchar(',');
+					}
+				
+			}
+		}
+		_putchar('\n');
+	}
 }
