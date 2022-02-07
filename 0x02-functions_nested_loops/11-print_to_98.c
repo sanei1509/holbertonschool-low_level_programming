854579590f53fe223c8347cadec09b58e58683f4
Santiago Neira <4373@holbertonschool.com>
2022-02-07 11:56:51 -0800

    try task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,11 +7,11 @@
  */
 void print_to_98(int n)
 {
-	if(n > 98)
+	if (n > 98)
 	{
-		for(n = n; n > 98; n--)
+		for (n = n; n > 97; n--)
 		{
-			if(n != 98)
+			if (n != 98)
 			{
 			printf("%d, ", n);
 			}
