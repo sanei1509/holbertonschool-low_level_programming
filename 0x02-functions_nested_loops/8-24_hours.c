794822a8d905a46b7f69c41783843c994436a96d
Santiago Neira <4373@holbertonschool.com>
2022-02-07 10:49:06 -0800

    task 9 print the time 00:00 until 23:59

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *jack_bauer - return deppend of sign of n
+ *
+ *Return: 0
+ *@c: number for evaluate
+ */
+void jack_bauer(void)
+{
+	int minutes;
+	int seconds;
+	for (minutes = 0; minutes <= 23; minutes++)
+	{
+		for (seconds = 0; seconds <= 59; seconds++)
+		{
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar(':');
+			_putchar((seconds / 10) + '0');
+			_putchar((seconds % 10) + '0');
+			if(minutes <= 23)
+			{
+				putchar('\n');
+			}
+		}
+	}
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *print_last_digit - return deppend of sign of n
+ *
+ *Return: 0
+ *@c: number for evaluate
+ */
+int print_last_digit(int c)
+{
+	int rt;
+}
