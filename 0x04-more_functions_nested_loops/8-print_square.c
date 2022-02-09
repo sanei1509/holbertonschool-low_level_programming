cf75645ef2e490153c58c3116573e01d1f72838e
Santiago Neira <4373@holbertonschool.com>
2022-02-09 10:45:22 -0800

    try task 8 sqare n times #

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,12 +1,20 @@
 #include "main.h"
 
 /**
- *print_line - print n times " " , ' \n' n times 
+ *print_square - print n times, \n' n times
  *@size : size of square
- * Return: 0
+ *Return: 0
  */
-
 void print_square(int size)
 {
-	
+	int y, x;
+
+	for (x = 0; x < size; ++x)
+	{
+		for (y = 0; y < size; ++y)
+		{
+			putchar('#');
+		}
+	putchar('\n');
+	}
 }
