68d2b54ed68197a67a1b40ff4e7f1ff616f7ef0b
sanei1509 <santiagoneira52@gmail.com>
2022-02-09 13:27:21 +0000

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *mul - multiplies two numbers
+ *@a: receive number
+ *@b: receive number
+ *Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int res;
+	
+	res = a * b;
+	return (res);
+}
