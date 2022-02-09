c4ef3c61a18a9a9ea8702f4f3a930d1bfa1bf8d2
Santiago Neira <4373@holbertonschool.com>
2022-02-09 10:19:32 -0800

    try task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,7 +5,7 @@
  *Return: 0
  */
 
-void print_line(int n)
+void print_diagonal(int n)
 {
 	int i, l;
 
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *print_line - print n times " " , ' \n' n times 
+ *@size : size of square
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	
+}
