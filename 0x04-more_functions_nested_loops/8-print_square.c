df0f38f83b1f317515566553f55a3a9e9228b70c
Santiago Neira <4373@holbertonschool.com>
2022-02-09 10:57:45 -0800

    try task 8 sqare n times #

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,19 +9,19 @@ void print_square(int size)
 {
 	int y, x;
 
+	if(size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
 	for (x = 0; x < size; ++x)
 	{
 		for (y = 0; y < size; ++y)
 		{
-			if (size <= 0)
-			{
-				_putchar('\n');
-			}
-			else
-			{
-				_putchar('#');
-			}
+			_putchar('#');
 		}
 	_putchar('\n');
+	}
 	}
 }
