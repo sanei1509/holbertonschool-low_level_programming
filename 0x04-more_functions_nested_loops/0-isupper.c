a88bef87203074de7794cc5dafaacdc3430b6acc
sanei1509 <santiagoneira52@gmail.com>
2022-02-09 15:33:06 +0000

    task 0 fixing

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,12 +8,12 @@
 
 int _isupper(int c)
 {
-	if(c >= 'a' && c <= 'z')
-	{
-		return (0);
-	}	
-	else if(c >= 'A' && c <= 'Z')
+	if(c >= 'A' && c <= 'Z')
 	{
 		return (1);
+	}	
+	else
+	{
+		return (0);
 	}
 }
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_line - print 0 to 9 but no 2 and 4
+ *Return: 0
+ */
+
+void print_line(int n)
+{
+	for ()
+	{
+
+	}
+	_putchar('-');
+	_putchar(' ');
+}
