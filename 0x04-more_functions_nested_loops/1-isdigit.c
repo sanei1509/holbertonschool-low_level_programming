c814bfcf9f26303f2de096916819af6aa86e6044
sanei1509 <santiagoneira52@gmail.com>
2022-02-09 13:10:03 +0000

    task 1 check if c is a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_isdigit - check if the entry is upper or lowercase
+ *@c: receive character
+ *Return: 0
+ */
+
+int _isdigit(int c)
+{
+	if((c >= 'a' && c <= 'z') || (c >= 'A' && c <='Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
