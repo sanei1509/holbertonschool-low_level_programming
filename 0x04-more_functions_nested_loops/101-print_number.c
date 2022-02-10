0da6860c9ce554d26e4c6487ff6697be2b0460a2
Santiago Neira <4373@holbertonschool.com>
2022-02-10 06:14:17 -0800

    try task11

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * *main - Prints out a string to 10.
+ * *Return: Returnn.
+ * */
+int main(void)
+{
+	long int i = 2;
+	long int n = 612852475143;
+
+	for (i = 2; i < n; i++)
+	{
+		if ((n % i) == 0)
+		{
+			n /= i;
+			i--;
+		}
+	}
+
+	printf("%ld\n", n);
+	return (0);
+}
