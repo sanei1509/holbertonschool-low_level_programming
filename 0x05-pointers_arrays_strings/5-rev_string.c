5a6d3c1c6cf702353b17f544e67b957903821f8f
Santiago Neira <4373@holbertonschool.com>
2022-02-11 07:42:44 -0800

    task 5 try one :)

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - print the string in reverse
+ * @s: The pointer value one
+ */
+void rev_string(char *s)
+{
+	int ld = 0;
+	int i;
+
+	while (s[ld] != '\0')
+	{
+		ld++;
+	}
+	for (i = ld; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
