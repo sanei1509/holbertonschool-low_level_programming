b28dde38337198e8e87e247ba4c2cd2576e4a589
Santiago Neira <4373@holbertonschool.com>
2022-02-11 07:28:17 -0800

    print reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - print the string in reverse
+ * @s: The pointer value one
+ */
+void print_rev(char *s)
+{
+	int ld = 0;
+	int i;
+
+	while (s[ld] != '\0')
+	{
+		ld++;
+	}
+	for (i = ld; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
