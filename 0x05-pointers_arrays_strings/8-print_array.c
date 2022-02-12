e338f24f97e4ea68e67dee94d8c0e3ac23050912
Santiago Neira <4373@holbertonschool.com>
2022-02-12 07:02:09 -0800

    print numbers of the array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,12 +9,16 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for(i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d", a[i]);
-		if(a[i] != n)
+		if (i != n - 1)
 		{
+		printf("%d", a[i]);
 		printf(", ");
+		}
+		else
+		{
+		printf("%d", a[i]);
 		}
 	}
 	printf("\n");
