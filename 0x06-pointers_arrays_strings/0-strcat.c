efba27be55f8335df18facb4b00ea2fc69d4fa14
sanei1509 <santiagoneira52@gmail.com>
2022-02-15 13:48:21 +0000

    concatenate two array of strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,7 +9,28 @@
 
 char *_strcat(char *dest, char *src)
 {
+	int n; int len = _strlen(dest);
 
+	for (n = 0; src[n] != '\0';n++, len++)
+	{
+		dest[len] = src[n];
+	}
 
-
+	return (dest);
 }
+
+/**
+*_strlen - swaps the value of two integers
+* @s: The pointer value one
+* Return: c
+*/
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+
+	return (c);
+}
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _strlen(char *s);
+
 int _putchar(char c);
 
 char *_strcat(char *dest, char *src);
@@ -15,7 +17,7 @@ void reverse_array(int *a, int n);
 
 char *string_toupper(char *);
 
-char *cap_string(char *);}
+char *cap_string(char *);
 
 char *leet(char *);
 
