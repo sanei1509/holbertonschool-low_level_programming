b55d50da51eb5409a42e415373a3473349515774
Santiago Neira <santiagoneira52@gmail.com>
2022-02-15 01:32:25 -0300

    Organizing everything to work tomorrow

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* *_strcat - function tha concatenate two strings
+* @dest : place where append de src string
+* @src: place of the string for append
+* Return : char
+*/
+
+char *_strcat(char *dest, char *src)
+{
+
+
+
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Working with more pointers arrays and strings
