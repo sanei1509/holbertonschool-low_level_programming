fedd4ede84fdfe268d720468bd3e5cd56e6a686a
sanei1509 <santiagoneira52@gmail.com>
2022-02-15 18:43:43 +0000

    revert string of elements of the array one by one since of half part

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* reverse_array - function that reverse the elements of an array
+* @a: array of elements
+* @n: number of elements in the a
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int temp;
+
+	for (; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i -1];
+		a[n -i -1] = temp;
+	}
+	
+}
