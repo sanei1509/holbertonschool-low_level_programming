5617cf8117b90de98f76f01e195c5b6598709684
sanei1509 <santiagoneira52@gmail.com>
2022-02-15 14:23:44 +0000

    print until n + solutions betty

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
-* *_strcat - function tha concatenate two strings
+**_strncat - function tha concatenate two strings
 * @dest : place where append de src string
 * @src: place of the string for append
+* @n: n is number until print src
 * Return: pointer to dest
 */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-	int n;
+	int i;
 	int len = _strlen(dest);
 
-	for (n = 0; src[n] != '\0'; n++, len++)
+	for (i = 0; src[i] != src[n]; i++, len++)
 	{
-		dest[len] = src[n];
+		dest[len] = src[i];
 	}
 
 	return (dest);
