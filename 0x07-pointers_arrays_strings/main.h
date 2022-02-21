46027ffeba1948933a39b7d4e0047869daa87a0a
sanei1509 <santiagoneira52@gmail.com>
2022-02-21 13:12:32 +0000

    correct output of task one copy bytes of memory

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-*_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
 
 char *_memcpy(char *dest, char *src, unsigned int n);
 
