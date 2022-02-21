4d9cc5fc03f87b59f5c9bd88fe28330058f7858e
sanei1509 <santiagoneira52@gmail.com>
2022-02-21 17:46:17 +0000

    task 5 test

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,18 +4,21 @@
 **_strstr - function that count ocurrences of first segmen with accept char
 *@haystack: string of input
 *@needle: characters that should compare with s
-*Return: the string with coincidences
+*Return: the stri	ng with coincidences
 */
 
 char *_strstr(char *haystack, char *needle)
 {
 	int i;
 
-	while (*needle != ' ')
+	while (*needle != '\0')
 	{
 		for (i = 0; haystack[i] != '\0'; i++)
 		{
-			return (needle);
+			if (haystack[i] == *needle)
+			{
+				return (haystack + i);
+			}
 		}
 		needle++;
 	}
