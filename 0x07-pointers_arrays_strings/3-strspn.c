af5cc55bd04e0fe53fe2f1acb4b1d119313e85b2
sanei1509 <santiagoneira52@gmail.com>
2022-02-21 15:44:48 +0000

    task 3 testing checker

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,23 +1,24 @@
 #include "main.h"
 
 /**
-*_strspn - |:wq
-*@s:
-*@accept:
+*_strspn - function that count ocurrences of first segmen with accept char
+*@s: string of input
+*@accept: characters that should compare with s
+*Return: length of the ocurrrences with the first segment
 */
 
 unsigned int _strspn(char *s, char *accept)
 {
 	int i;
 	int length = 0;
-	
+
 	while (*s != '\0')
 	{
 		for (i = 0; accept[i] != '\0'; i++)
 		{
 			if (*s != ' ')
 			{
-				if(accept[i] == *s)
+				if (accept[i] == *s)
 				{
 					length++;
 				}
