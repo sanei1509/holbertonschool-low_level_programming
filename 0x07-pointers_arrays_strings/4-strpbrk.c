398280165ea4e662411d3c7fe007a687649523e8
sanei1509 <santiagoneira52@gmail.com>
2022-02-21 16:26:32 +0000

    task 4 return only the characters with coincidence

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+**_strpbrk - function that count ocurrences of first segmen with accept char
+*@s: string of input
+*@accept: characters that should compare with s
+*Return: the string with coincidences
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s != ' ')
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (accept[i] == *s)
+			{
+				return (s);
+			}
+			else
+			{
+				return ('\0');
+			}
+		}
+		s++;
+	}
+	return (s);
+}
