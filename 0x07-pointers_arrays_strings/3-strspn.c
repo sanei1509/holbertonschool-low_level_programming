05bccc3ee87a2c01dcfb0067c482f5bce5bc8313
sanei1509 <santiagoneira52@gmail.com>
2022-02-21 15:39:49 +0000

    task 3 testing checker

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,12 +9,22 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i;
+	int length = 0;
 	
-	while(*s != '\0')
+	while (*s != '\0')
 	{
-		for()
+		for (i = 0; accept[i] != '\0'; i++)
 		{
+			if (*s != ' ')
+			{
+				if(accept[i] == *s)
+				{
+					length++;
+				}
+			}
 
 		}
+		s++;
 	}
+	return (length - 2);
 }
