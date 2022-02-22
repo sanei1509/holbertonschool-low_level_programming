de3e7824ac321dfedda6429aaa005d0fad4726dc
Santiago Neira <santiagoneira52@gmail.com>
2022-02-21 23:04:58 -0300

    task 7 sum the diagonal of a square matrix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,5 +8,21 @@
 */
 void print_diagsums(int *a, int size)
 {
-	
+	int diagonal_one = 0;
+	int diagonal_two = 0;
+	int row, i;
+
+	for (row = 0; row < size; row++)
+	{
+		i = (row * size) + row;
+		diagonal_one += a[i];
+	}
+
+	for (row = 1; row <= size; row++)
+	{
+		i = (row * size) - row;
+		diagonal_two += a[i];
+	}
+
+	printf("%d, %d\n", diagonal_one, diagonal_two);
 }
