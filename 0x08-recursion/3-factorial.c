a5fac049834439b215dbc0dadc193371dc1335c4
sanei1509 <santiagoneira52@gmail.com>
2022-02-23 16:46:28 +0000

    task 3 a factorial of n

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
-* int factorial - print the factorial of given number
+* factorial - print the factorial of given number
 * @n: number to do a factorial
 * Return: Always 0.
 */
 
 int factorial(int n)
 {
-
 	if (n >= 1)
 	{
 		return (n * factorial(n - 1));
 	}
-	else if(n < 0)
+	else if (n < 0)
 	{
 		return (-1);
 	}
 	else
-		return(1);
+		return (1);
 }
