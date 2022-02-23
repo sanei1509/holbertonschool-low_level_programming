26dcd6e0974e750630772cb61f3e8ec27a337afa
sanei1509 <santiagoneira52@gmail.com>
2022-02-23 16:09:47 +0000

    task 2 calc the lenght of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_strlen_recursion - return the lenght of a string
+*@s: received a char
+*Return: the lenght of the string
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
