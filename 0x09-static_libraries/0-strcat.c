e975a71403b21d3c828bf80a66f20ea2b2d8b6ef
sanei1509 <santiagoneira52@gmail.com>
2022-02-25 20:17:40 +0000

    task 1 and 2 of create a library commands ar and gcc + options

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* *_strcat - function tha concatenate two strings
+* @dest : place where append de src string
+* @src: place of the string for append
+* Return: pointer to dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int n;
+	int len = _strlen(dest);
+
+	for (n = 0; src[n] != '\0'; n++, len++)
+	{
+		dest[len] = src[n];
+	}
+
+	return (dest);
+}
+
+/**
+*_strlen - swaps the value of two integers
+* @s: The pointer value one
+* Return: c
+*/
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+
+	return (c);
+}
+
