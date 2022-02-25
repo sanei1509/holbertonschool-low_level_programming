81b953a5e9d6c2bc2fb730cc2fbf19a8634b36ed
sanei1509 <santiagoneira52@gmail.com>
2022-02-25 15:22:27 +0000

    correction of output task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,13 +9,13 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc <= 2)
+	if (argc - 1 < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	else
-		printf("%d \n", atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
 	return (0);
 }
