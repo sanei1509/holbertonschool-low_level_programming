036710ec2898475787103937e98c3412fb8656d8
sanei1509 <santiagoneira52@gmail.com>
2022-02-25 17:57:00 +0000

    task 4 test of output

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <ctype.h>
 
 /**
 *main - program that adds positive numbers
@@ -9,6 +10,30 @@
 
 int main(int argc, char *argv[])
 {
+	int i, j;
+	int res = 0;
+	char *arr;
 
+	if (argc - 1 == 0)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			arr = argv[i];
+			for (j = 0; arr[j] != '\0'; j++)
+			{
+				if (isdigit(arr[j]) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			res += atoi(argv[i]);
+		}
+		printf("%d\n", res);
+	}
 	return (0);
 }
