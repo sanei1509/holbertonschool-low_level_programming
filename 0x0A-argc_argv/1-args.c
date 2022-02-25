3367eb9c5644c8062e33ec2d7b95fa844bee19ed
sanei1509 <santiagoneira52@gmail.com>
2022-02-25 13:24:54 +0000

    task 1 print the numbers of argumentson the command line

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*main - print the number of arguments passed
+*@argc:
+*@argv:
+*Return: 0
+*/
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d \n", argc);
+	return (0);
+}
