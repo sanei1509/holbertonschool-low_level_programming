01bac7922fe93fcf4cc3ac163fef855b1debb8d7
sanei1509 <santiagoneira52@gmail.com>
2022-02-25 13:05:14 +0000

    task 0 print the name of program withargv[0]

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*main -write program that prints his name, followed by a new line
+*@argc: count the quantity of arguments
+*@argv: different data in a vector
+*Return: 0
+*/
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s \n", argv[0]);
+	return (0);
+}
