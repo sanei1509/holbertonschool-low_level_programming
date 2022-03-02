888bd90c709dcdab5b42c11fdb747a683991601d
Santiago Neira <santiagoneira52@gmail.com>
2022-03-02 15:06:44 -0300

    betty corrections

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,22 +17,13 @@ char *str_concat(char *s1, char *s2)
 	*check the input
 	*/
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	while (*(s1 + i))
-	{
 		len_one++, i++;
-	}
-
 	while (*(s2 + j))
-	{
 		len_two++, j++;
-	}
 	len_two++;
 	/*no le añadimos el null a s1*/
 
