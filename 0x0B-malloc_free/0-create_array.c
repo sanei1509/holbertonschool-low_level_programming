979f5779e335a3c7e33e11d253723b7bbbcde569
Santiago Neira <santiagoneira52@gmail.com>
2022-03-02 10:53:27 -0300

    task 1 use malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,12 +18,17 @@ char *create_array(unsigned int size, char c)
 	{
 		return (0);
 	}
+
+	if (ar == '\0')
+	{
+		return (0);
+	}
 	else
 	{
-		for (i = 0; i < size; i++)
-		{
-			ar[i] = c;
-		}
+	for (i = 0; i < size; i++)
+	{
+		ar[i] = c;
+	}
 		return (ar);
 	}
 }
