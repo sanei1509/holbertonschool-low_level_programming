0f1a91a0bae4633dbc83b6400d9fdbbfc02dd82c
Santiago Neira <santiagoneira52@gmail.com>
2022-03-02 10:39:26 -0300

    task 1 use malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+**create_array - function that create a new array with a especific character
+*@size: receive a size of array
+*@c: receive a especific char
+*RETURN: pointer to a char or null
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *ar;
+
+	ar = malloc(c * size);
+
+	if (size == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			ar[i] = c;
+		}
+		return (ar);
+	}
+	
+}
