f948691bd6dbe8da42f2a19c38ccd8b15c0cbe8b
Santiago Neira <santiagoneira52@gmail.com>
2022-03-02 15:01:32 -0300

    correction task 10

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,9 +16,13 @@ char *str_concat(char *s1, char *s2)
 	*Necesitamos la longitud de s1 y s2
 	*check the input
 	*/
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return (" ");
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 	while (*(s1 + i))
 	{
