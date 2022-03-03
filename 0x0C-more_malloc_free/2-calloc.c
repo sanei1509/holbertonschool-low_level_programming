a68867965bdf73aff5b3603f9c73771ffa42a717
sanei1509 <santiagoneira52@gmail.com>
2022-03-03 17:42:02 +0000

    task 2 create a calloc() with malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (size == 0)
 		return (NULL);
 
-	for (i = 0; i < nmemb + 1; i++)
+	for (i = 0; i < (nmemb * size); i++)
 	{
 		ar[i] = 0;
 	}
