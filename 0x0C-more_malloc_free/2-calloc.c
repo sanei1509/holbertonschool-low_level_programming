fab3f7bf8ec37a93990999185a481d5d39fe9cbe
sanei1509 <santiagoneira52@gmail.com>
2022-03-03 17:17:27 +0000

    task 2 create a calloc() with malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ **_calloc - allocates memory for an array nmemb of size elements
+ *@nmemb: num of elements
+ *@size: size of elements
+ *Return: a pointer to the allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ar;
+	unsigned int i;
+
+	ar = malloc(nmemb * size);
+	if (ar == NULL)
+		return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+	{
+		ar[i] = 0;
+	}
+
+	return (ar);
+}
