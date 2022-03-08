bca51085e754d512b3ed8001228709cda9fc9c2d
sanei1509 <santiagoneira52@gmail.com>
2022-03-08 15:28:04 +0000

    fix problems of betty

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*print_dog - print a struct dog
+*@struct dog: receive a structure of dog
+*@d: receive a pointer
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d => name)
+	{
+		printf(nil);
+	}
+	if(d => name == NULL)
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,12 +2,21 @@
 #define _DEFINE_DOG
 
 #include <stdio.h>
-
+/**
+ *struct dog - atributes of dog
+ *@name: pointer to a name
+ *@age: receive age
+ *@owner: pointer to a owner
+ */
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
+
+void print_dog(struct dog *d);
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
