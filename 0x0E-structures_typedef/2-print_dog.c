a2f88e26b6bc80108a1a1bb626ad2742659dd853
sanei1509 <santiagoneira52@gmail.com>
2022-03-08 17:10:31 +0000

    fix problem of sign - age

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 	{
 		if (d->name == NULL)
 		{
-			printf("Name: (nil)");
+			printf("Name: (nil)\n");
 		}
 			else
 				printf("Name: %s\n", d->name);
@@ -20,7 +20,7 @@ void print_dog(struct dog *d)
 		printf("Age: %f\n", d->age);
 		if (d->owner == NULL)
 		{
-			printf("Owner: (nil)");
+			printf("Owner: (nil)\n");
 		}
 			else
 				printf("Owner: %s\n", d->owner);
