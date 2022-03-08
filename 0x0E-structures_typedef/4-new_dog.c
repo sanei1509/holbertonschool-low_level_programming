737e4e486205c95173cd2b9a42f128c0e013cdf1
sanei1509 <santiagoneira52@gmail.com>
2022-03-08 18:37:41 +0000

    try of task 4 but not :(

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,8 +5,12 @@
  *@name: receive name
  *@age: receive age
  *@owner: receive owner name
+ *Return: a new type of dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
 }
