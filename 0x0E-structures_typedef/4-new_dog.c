125f1ecf7bb20549801656260964b98d5652143e
sanei1509 <santiagoneira52@gmail.com>
2022-03-08 18:35:22 +0000

    try of task 4 but not :(

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+
+/**
+ **new_dog - function that creates a new dog
+ *@name: receive name
+ *@age: receive age
+ *@owner: receive owner name
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+}
