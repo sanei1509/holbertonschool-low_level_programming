025bdc33d1015615e3221601a6771ab0fd1412cf
sanei1509 <santiagoneira52@gmail.com>
2022-03-09 22:29:44 +0000

    trying task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	/* user tries divide or module of 0*/
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && (argv[3] == 0))
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && (atoi(argv[3]) == 0))
 	{
 		printf("Error\n");
 		exit(100);
