147b0354af7cbbd5a112ece46752d74bc929da03
sanei1509 <santiagoneira52@gmail.com>
2022-03-10 16:36:07 +0000

    fixing print nil

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,11 +19,17 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		string = va_arg(params, char *);
-
+		if (string == NULL)
+		{
+		printf("(nil)");
+		}
+		else
+		{
 		if (i < n - 1 && separator != NULL)
 			printf("%s%s", string, separator);
 		else
 			printf("%s", string);
+		}
 	}
 	printf("\n");
 	va_end(params);
