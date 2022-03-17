65a0ac3dae30f488de1e9896e6cfe98ff1015dc5
Santiago Neira <santiagoneira52@gmail.com>
2022-03-17 16:52:24 +0000

    task 2 -create new node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,14 +4,15 @@
  **add_node - function that create a new node at the beginning
  *@head: pointer to a first node of the list_t
  *@str: need to be duplicated
+ *Return: direction of new node
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *temp = NULL;
-	
+
 	temp = (malloc(sizeof(list_t)));
 
-	if(temp != NULL)
+	if (temp != NULL)
 	{
 	/*asigno a temp el lugar dehead*/
 	temp->next = (*head);
