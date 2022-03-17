82d483bdb99cbf1ea130f002bed76556189c9a53
Santiago Neira <santiagoneira52@gmail.com>
2022-03-17 15:49:03 +0000

    task one return the number of element of the list_t

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *list_len - list the num of elements of nodes
+ *@h: pointer to first node of list_h(head)
+ *Return: num of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
