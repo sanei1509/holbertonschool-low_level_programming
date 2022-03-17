72898211750854e4f22936fc6528999465ee3db8
Santiago Neira <santiagoneira52@gmail.com>
2022-03-17 09:54:03 -0300

    header, test file of task 1 and readme

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+*print_list - print elements of a list
+*@h: pointer to a list
+*Return: number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	
+	
+	/*return num of nodes*/
+}
