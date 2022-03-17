11ecfaacb05701c4d4c4d1266312bbbc10c8fcae
Santiago Neira <santiagoneira52@gmail.com>
2022-03-17 19:59:23 +0000

    task 4 advanced

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,9 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ *myStartupFun - Print text before the execute of header file
+ */
 void myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
