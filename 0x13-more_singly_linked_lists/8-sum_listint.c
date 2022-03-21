2cbd8a414b09b7cf81eda81557c361653c73e40c
Santiago Neira <santiagoneira52@gmail.com>
2022-03-21 19:58:46 -0300

    try task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+*sum_listint - return the sum of all the data (n) -> list_name
+*@head: pointer to a first node of the list
+*Return: 0 or result of the sum
+*/
+
+int sum_listint(listint_t *head)
+{
+	//allocate node
+	listint_t node_;
+
+	/*put in the data*/
+	node_-> = new_data;
+
+	/**/
+}
