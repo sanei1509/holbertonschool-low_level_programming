e2b73c7d30f8e59e8bbbbde1ef3fbc887c8f2435
Santiago Neira <santiagoneira52@gmail.com>
2022-03-22 09:41:12 -0300

    fix problems with task 3

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,15 +7,15 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *aux_1, *aux_2;
+	listint_t *aux_2;
 
 	aux_2 = *head;
-
+	/*comprobamos que hay head*/
+	if(*head == NULL)
+		return;
 	while (aux_2)
 	{
-		aux_1 = aux_2->next;
-		free(aux_2);
-		aux_2 = aux_1;
+		aux_1 = aux_2->ne;
 	}
 	*head = NULL;
 }
