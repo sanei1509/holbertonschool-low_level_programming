931d6c53db962e9f2eb972238f9818729c0ef878
Santiago Neira <santiagoneira52@gmail.com>
2022-03-22 12:32:26 -0300

    trying task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,27 @@
 #include "lists.h"
 
 /**
+*getNode - crate and return node
+*@n: integers n
+*Return: newnode
+*/
+listint_t *getNode(int n)
+{
+	listint_t *newNode;
+	
+	newNode = malloc(sizeof(listint_t));
+
+	if(newNode != NULL)
+	{
+	/**inserting data*/
+	newNode->n = n;
+	newNode->next = NULL;
+	return newNode;
+	}
+	return(NULL);
+}
+
+/**
 **insert_nodeint_at_index - put the node in a given position
 *@head: pointer to a pointer to a first node
 *@idx: given position
@@ -10,7 +31,7 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *aux = *head;
+	listint_t *temp;
 	unsigned int size = 0;
 
 	if (idx < 1 || idx > size + 1)
@@ -19,13 +40,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (idx == 0)
 		{
-			aux->next = *head;
-			*head = aux;
-			aux->n = n;
+		/*adding node at idx*/
+			temp = getNode(n);
+			temp->next = *head;
+			*head = temp;
 		}
 		else
 		head = &(*head)->next;
 	}
 	size++;
-	return (aux);
+	return(temp);
 }
