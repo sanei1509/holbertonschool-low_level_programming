52a2db93a724581ed077c9096bd95b65cd8636a3
Santiago Neira <santiagoneira52@gmail.com>
2022-03-22 09:27:24 -0300

    fix problems with task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,27 +14,28 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	/*guardando espacio para nuevo nodo*/
 	new_node = malloc(sizeof(listint_t));
-
+	/*comprobar si es nulo*/
+	if (new_node != NULL)
+	{
+		new_node->next = NULL;
+		new_node->n = n;
+	}
+	else
+		return (NULL);
 	/*si solo hay un nodo*/
 	if (*head == NULL)
 	{
 		*head = new_node;
 	}
+	else
+	{
+	/*recorrer hasta el ultimo*/
 	while (aux->next != NULL)
 	{
 		aux = aux->next;
 	}
+	}
 	/*igualar el nuevo nodo al que queda al salir del while*/
 	aux->next = new_node;
-	if (new_node != NULL)
-	{
-	/*convertimos el nuevo nodo a ultimo nodo*/
-	new_node->next = NULL;
-	new_node->n = n;
-	}
-	else
-	{
-		return (NULL);
-	}
 	return (new_node);
 }
