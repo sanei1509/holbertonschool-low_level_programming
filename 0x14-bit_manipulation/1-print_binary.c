ec88180b515d13e74ae8c36d250a63ee04c49d69
Santiago Neira <santiagoneira52@gmail.com>
2022-03-24 20:56:47 -0300

    task one with recursion

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,18 +8,7 @@
 
 void print_binary(unsigned long int n)
 {
-
-	if (n == 0)
-		_putchar(48);
-
-	while (n)
-	{
-		if ((n & 1) == 0)
-			_putchar('0');
-		else
-			_putchar('1');
-		/*right shif 'n' by 1 */
-		n >>= 1;
-	}
-
+	if (n > 1)
+		print_binary(n >>= 1);
+		_putchar((n & 1) + 48);
 }
