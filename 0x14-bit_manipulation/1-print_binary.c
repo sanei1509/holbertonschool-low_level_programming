a2d41fb4fbd619737779d025b974f0bb9dbe3390
Santiago Neira <santiagoneira52@gmail.com>
2022-03-24 19:00:13 +0000

    test task one

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_binary - binary representation of a number
+ *@n: a big positive number
+ *Return: the number converted to binary
+ */
+
+void print_binary(unsigned long int n)
+{
+
+	if (n == 0)
+		_putchar(48);
+
+	while (n)
+	{
+		if ((n & 1) == 0)
+			putchar('0');
+		else
+			putchar('1');
+
+		/*right shif 'n' by 1 */
+		n >>= 1;
+	}
+
+}
