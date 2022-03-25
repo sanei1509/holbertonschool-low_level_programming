ea47588292204a1fcd860669fb96e943339e2224
Santiago Neira <santiagoneira52@gmail.com>
2022-03-24 22:04:04 -0300

    testing task two

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,8 +11,8 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int num_bin = (n >> index) & 1;
 
-	if (n)
-		return (num_bin);
+	if (index > 64)
+		return (-1);
 
-	return (-1);
+	return (num_bin);
 }
