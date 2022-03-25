701a7cc2d47a8d715f8b74712c8cd7cbb12cefe4
Santiago Neira <santiagoneira52@gmail.com>
2022-03-24 23:49:34 -0300

    correct check 11 of task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,7 +11,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int count = 0, i;
 
-	for (i = n ^ m; i != 0; i = i >> 1)
+	for (i = n ^ m; i != 0 && i > 0; i = i >> 1)
 		count += i & 1;
 
 	return (count);
