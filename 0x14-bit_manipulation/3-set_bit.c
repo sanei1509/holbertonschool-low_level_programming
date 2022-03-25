caeaef4f9f255ef2429f9c0cf16f183d3b8b591d
Santiago Neira <santiagoneira52@gmail.com>
2022-03-24 22:37:23 -0300

    changing a given bit of a binary number

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,18 +4,15 @@
 *set_bit - change bit of a given position
 *@n: pointer to a number binary
 *@index: position of a bit to change
+*@Return: 1 if work -1 is not
 */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int mask_op;
-
 	if (index > 64)
 		return (-1);
 
-	mask_op = 1 << index;
-
-	*n = (*n | mask_op);
+	*n = (*n | 1 << index);
 
 		return (1);
 
