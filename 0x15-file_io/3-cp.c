700ca8e9548ae8306ba3e3049378395a27585593
Santiago Neira <santiagoneira52@gmail.com>
2022-03-28 21:50:58 +0000

    first try of task 4

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ *main - copy file 1 into file 2
+ *@argc: count of arguments
+ *@argv: array with arguments
+ * Return: 1
+ */
+
+int main(int argc, char *argv[])
+{
+	FILE *file_src,  *file_dest;
+	char ch;
+
+	/*exit 97*/
+	/*incorrect number of args*/
+	if (argc != 3)
+		exit(97);
+
+	file_src = fopen(argv[1], "r");
+
+	if (file_src == NULL)
+		dprintf(2, "Error: Can't read from file %s", argv[1]), exit(98);
+	/*creamos el primer archivo*/
+	open(argv[1], O_RDWR | O_CREAT | O_TRUNC, 0664);
+
+	file_dest = fopen(argv[2], "w");
+	if (file_dest == NULL)
+		dprintf(2, "Error: Can't read from file %s", argv[2]), exit(98);
+
+	/*Leemos y copiamos el archivo 1 file_1*/
+	while ((ch = fgetc(file_src)) != EOF)
+		fputc(ch, file_dest);
+	/*exit 99*/
+
+	/*exit 100*/
+	fclose(file_src);
+	fclose(file_dest);
+	return (1);
+
+}
