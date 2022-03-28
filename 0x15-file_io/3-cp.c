34b534cfd393513dedbc7323ad86106fc1f22c61
Santiago Neira <santiagoneira52@gmail.com>
2022-03-28 22:51:15 +0000

    trying task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,7 +10,9 @@
 int main(int argc, char *argv[])
 {
 	FILE *file_src,  *file_dest;
-	char ch;
+	char ch, *buf1;
+
+	buf1 = malloc(sizeof(char) * 1024);
 
 	/*exit 97*/
 	/*incorrect number of args*/
@@ -18,21 +20,26 @@ int main(int argc, char *argv[])
 		exit(97);
 
 	file_src = fopen(argv[1], "r");
-
+	if (buf1 == NULL)
+		dprintf(2, "Can't write to %s", argv[1]), exit(99);
 	if (file_src == NULL)
 		dprintf(2, "Error: Can't read from file %s", argv[1]), exit(98);
 	/*creamos el primer archivo*/
 	open(argv[1], O_RDWR | O_CREAT | O_TRUNC, 0664);
 
 	file_dest = fopen(argv[2], "w");
+
+	if (buf1 == NULL)
+		dprintf(2, "Can't write to %s", argv[2]), exit(99);
 	if (file_dest == NULL)
 		dprintf(2, "Error: Can't read from file %s", argv[2]), exit(98);
 
 	/*Leemos y copiamos el archivo 1 file_1*/
 	while ((ch = fgetc(file_src)) != EOF)
+	{
 		fputc(ch, file_dest);
-	/*exit 99*/
-
+		ch = fgetc(file_src);
+	}
 	/*exit 100*/
 	fclose(file_src);
 	fclose(file_dest);
