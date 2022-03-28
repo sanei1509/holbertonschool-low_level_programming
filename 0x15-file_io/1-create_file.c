756deaf08a6a0adb509d795320fe169aae45ad98
Santiago Neira <santiagoneira52@gmail.com>
2022-03-28 18:26:31 +0000

    task 1 - fixing problems

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,23 +13,28 @@ int create_file(const char *filename, char *text_content)
 	int file_d, write_rt;
 
 	if (filename == NULL)
+	{
+		close(file_d);
 		return (-1);
-
+	}
 	if (text_content == NULL)
 		text_content = "";
 
 	file_d = open(filename, O_CREAT | O_RDONLY | O_TRUNC, 0600);
-
 	if (file_d == -1)
+	{
 		return (-1);
+	}
 
 	len_content = strlen(text_content);
 
 	write_rt = write(file_d, text_content, len_content);
 
 	if (write_rt == -1)
+	{
+		close(file_d);
 		return (-1);
-
+	}
 	close(file_d);
 	return (1);
 }
