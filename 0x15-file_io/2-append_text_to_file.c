cbe2bdb0c547a70c7847b0eebb3715c50febe5ca
Santiago Neira <santiagoneira52@gmail.com>
2022-03-28 18:52:45 +0000

    task 2 - fix problems of text content empty

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		text_content = "";
+		return (1);
 
 	file_d = open(filename, O_RDONLY | O_APPEND);
 
