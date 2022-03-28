c307505ab9e4d3a8deaef581a63a086101cf0414
Santiago Neira <santiagoneira52@gmail.com>
2022-03-28 18:22:56 +0000

    task 1 - create a new file with especific permissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,12 +2,34 @@
 
 /**
  *create_file - create a file
- *@filename:
- *@text_content
+ *@filename: name of the file
+ *@text_content: content of the new file
  *Return: 1 on succes | -1 on fails
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	
+	int len_content = 0;
+	int file_d, write_rt;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	file_d = open(filename, O_CREAT | O_RDONLY | O_TRUNC, 0600);
+
+	if (file_d == -1)
+		return (-1);
+
+	len_content = strlen(text_content);
+
+	write_rt = write(file_d, text_content, len_content);
+
+	if (write_rt == -1)
+		return (-1);
+
+	close(file_d);
+	return (1);
 }
