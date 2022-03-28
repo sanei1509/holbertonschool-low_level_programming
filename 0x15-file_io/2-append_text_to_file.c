c73a4c15cf8372c6cafddd8bca83220cefb31795
Santiago Neira <santiagoneira52@gmail.com>
2022-03-28 18:45:52 +0000

    task 2 - test of output

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ *append_text_to_file - appends text at the end of a file
+ *@filename: name of the file
+ *@text_content: content to copy
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int len_content = 0;
+	int file_d, write_rt;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	file_d = open(filename, O_RDONLY | O_APPEND);
+
+	if (file_d == -1)
+	{
+		return (-1);
+	}
+
+	len_content = strlen(text_content);
+
+	write_rt = write(file_d, text_content, len_content);
+
+	if (write_rt == -1)
+	{
+		close(file_d);
+		return (-1);
+	}
+	close(file_d);
+	return (1);	
+}
