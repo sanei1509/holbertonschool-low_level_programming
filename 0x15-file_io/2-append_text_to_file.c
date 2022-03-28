a973f59d77c960d955f5823535c4a15b5e98a845
Santiago Neira <santiagoneira52@gmail.com>
2022-03-28 19:00:36 +0000

    task 2 - fix problems of flag open

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		return (1);
 
-	file_d = open(filename, O_RDONLY | O_APPEND);
+	file_d = open(filename, O_WRONLY | O_APPEND);
 
 	if (file_d == -1)
 	{
