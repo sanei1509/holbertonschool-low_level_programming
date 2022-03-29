74d412a5b6e222c9a7d2afb805c5e5b7c32c8345
Santiago Neira <santiagoneira52@gmail.com>
2022-03-29 16:55:51 -0300

    trying again create the cp command

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  *main - copy file 1 into file 2
  *@argc: count of arguments
@@ -9,9 +8,9 @@
 
 int main(int argc, char *argv[])
 {
-	FILE *file_src,  *file_dest;
-	int count, writes, close_d;
+	int file_src,  file_dest;
 	char buf[1024];
+	int read_res, write_res, close_d;
 
 	/*incorrect number of args*/
 	if (argc != 3)
@@ -19,23 +18,29 @@ int main(int argc, char *argv[])
 
 	file_src = open(argv[1], O_RDONLY);
 	if (file_src == -1)
-		dprintf(2, "Error: Can't read from file %s", argv[1]), exit(98);
-
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	file_dest = open(argv[1], O_RDWR | O_CREAT | O_TRUNC, 0664);
-
+	/*exit 99*/
 	if (file_dest == -1)
-		dprintf(2, "Error: Can't read from file %s", argv[2]), exit(98);
-
+		dprintf(2, "Error: Can't write %s\n", argv[2]), exit(99);
 	/*Leemos y copiamos el archivo 1 file_1*/
-	
+	read_res = read(file_src, buf, 1024);
+	while (read_res != 0)
+	{
+		if (read_res == -1)
+			dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	/*escribimos lo leido dentro del file_2 de en buf hasta count*/
+		write_res = write(file_dest, buf, read_res);
+		if (write_res)
+			dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
 	/*exit 100*/
-	close_d = close(src);
-	if (close == -1)
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from), exit(100);
-	close_d = close(dest);
-	if (close == -1)
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to), exit(100);
+	close_d = close(file_src);
+	if (close_d == -1)
+		dprintf(2, "Error: Can't close fd %d\n", file_src), exit(100);
+	close_d = close(file_dest);
+	if (close_d == -1)
+		dprintf(2, "Error: Can't close fd %d\n", file_dest), exit(100);
 
-	return (1);
-
+	return (0);
 }
