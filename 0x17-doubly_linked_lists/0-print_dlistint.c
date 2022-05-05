3c5186250603f725ab71322730674d95387313a3
Santiago Neira <santiagoneira52@gmail.com>
2022-05-05 15:10:07 +0000

    printelements of the linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ *print_dlistint - print all the element of the doubly linked list
+ *@h : is a pointer to first node
+ *Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t contador = 0;
+
+	/*aseguramos que la lista tenga nodos*/
+	while (h != NULL)
+	{
+		/*verificamos que haya data*/
+		if (h->n)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			contador++;
+		}
+	}
+	return (contador);
+}
