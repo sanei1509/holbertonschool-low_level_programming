f018a7197149f2864a1dce70ebe607203ef0f44e
Santiago Neira <santiagoneira52@gmail.com>
2022-05-05 15:11:51 +0000

    print len of the list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ *dlistint_len - return the number of elements in a linked list
+ *@h : is a pointer to first node
+ *Return: the len of the linked lists
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t contador = 0;
+
+	/*aseguramos que la lista tenga nodos*/
+	while (h != NULL)
+	{
+		/*verificamos que haya data*/
+		if (h->n)
+		{
+			h = h->next;
+			contador++;
+		}
+	}
+	return (contador);
+}
