a59451bb120b2f77d4576b2e24b16a8fbeea0aa3
Santiago Neira <santiagoneira52@gmail.com>
2022-05-05 16:00:03 +0000

    correct in a pointer prev

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- **add_node - add a new node at the beginning
+ **add_nodeint - add a new node at the beginning
  *@head: pointer to a pointer que apunta al primer node
  *@n: data integers in the nodes
  *Return: direccion del nuevo elemento, or NULL
@@ -12,19 +12,28 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node = NULL;
 
 	new_node = malloc(sizeof(dlistint_t));
-	
-	if(new_node == NULL)
-		return(NULL);
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
+	}
 
 	if (new_node != NULL)
 	{
 		/*le doy la data que me pasan al nuevo elemento*/
 		new_node->n = n;
-		new_node->next = (*head);
+		new_node->next = *head;
 		*head = new_node;
 	}
-	else
-		return (NULL);
 	
-	return (*head);
+	return (new_node);
 }
