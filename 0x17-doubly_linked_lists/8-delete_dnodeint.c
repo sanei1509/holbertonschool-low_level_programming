8c39456313ba0b017dcd3f6f6ddc2be21f599f37
Santiago Neira <santiagoneira52@gmail.com>
2022-05-05 19:33:07 +0000

    try sfix task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,28 +1,6 @@
 #include "lists.h"
 
 /**
- *deleteNode - delete no in a doubly linked list
- *@head_ref: pointer to head node pointer.
- *@del: pointer to node to be deleted.
- */
-void deleteNode(dlistint_t **head_ref, dlistint_t *del)
-{
-	if (*head_ref == NULL || del == NULL)
-		return;
-
-	if (*head_ref == del)
-		*head_ref = del->next;
-
-	if (del->next != NULL)
-		del->next->prev = del->prev;
-
-	if (del->prev != NULL)
-		del->prev->next = del->next;
-
-	free(del);
-}
-
-/**
 *delete_dnodeint_at_index - Eliminar un nodo en posicion dada
 *@head: pointer to a pointer pointing a first node
 *@index: indice of node to delete
@@ -36,14 +14,14 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i = 0;
 
 	/*si esta vacia*/
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return (-1);
 	/*guardamos el primer nodo*/
 	aux = *head;
 
 	if (index == 0)
 	{
-		*head = aux->next;
+	*head = (*head)->next;
 		free(aux);
 		if (*head != NULL)
 			(*head)->prev = NULL;
@@ -60,8 +38,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	free(aux->next);
 	aux->next = next;
-
-	deleteNode(head, aux);
 
 	return (1);
 }
