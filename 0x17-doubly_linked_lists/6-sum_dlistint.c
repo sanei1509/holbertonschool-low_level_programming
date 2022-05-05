692f19be94da9c9c0a5b9e608f8ff0a99f479452
Santiago Neira <santiagoneira52@gmail.com>
2022-05-05 16:53:11 +0000

    sum all of the data

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+*sum_dlistint - devuelve la suma de toda la data(n)
+*@head: pointer to a first node of the list
+*Return: 0 or result of the sum
+*/
+
+int sum_dlistint(dlistint_t *head)
+{
+	listint_t *ptr = head;
+
+	int sum = 0;
+
+	while (ptr != NULL)
+	{
+		sum += ptr->n;
+		ptr = ptr->next;
+	}
+
+	/*si hay valores va a devolver el valor y sino 0*/
+	return (sum);
+}
