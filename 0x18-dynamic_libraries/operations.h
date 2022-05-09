cc2a5987373d6c796c959b71f5fec0e64582c7c8
Santiago Neira <santiagoneira52@gmail.com>
2022-05-09 17:31:54 +0000

    create the header of operation functions

---
+++ 0x18-dynamic_libraries/operations.h
@@ -0,0 +1,10 @@
+#ifndef _OPERATIONS_H
+#define _OPERATIONS_H
+
+/*prototype of the functions write in python*/
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+#endif
