afac7549512d76e4f764a1d2ada5058b41fc0dd4
Santiago Neira <santiagoneira52@gmail.com>
2022-08-25 19:34:21 +0000

    create header for the project and try task 1

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,1 +1,25 @@
+#include "search_algos.h"
 
+/**
+ * linear_search - Buscar un elemento en un array usando busqueda lineal
+ * @array: Array donde buscar
+ * @size: length del array
+ * @value: valor del elemento buscado
+ *
+ * Return: si falla -1, en otro caso el [index] valor encontrado
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!array || size == 0)
+		return (-1);
+	/*recorro el array hasta encontrar coincidencia*/
+	for (i = 0; i < size; i++)
+	{
+		if (value == array[i])
+			return (i)
+	}
+	return (-1);
+}
