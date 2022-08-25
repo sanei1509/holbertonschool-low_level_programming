eb93ee1116857a45ce74f76b0bfc0d7cdbd86a10
Santiago Neira <santiagoneira52@gmail.com>
2022-08-25 22:50:50 +0000

    fix task one

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,8 +18,9 @@ int linear_search(int *array, size_t size, int value)
 	/*recorro el array hasta encontrar coincidencia*/
 	for (i = 0; i < size; i++)
 	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (value == array[i])
-			return (i)
+			return (i);
 	}
 	return (-1);
 }
